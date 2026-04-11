22960438b18c230ac9516cf495b1d095eaa479b5
Schuyler Lewis <schuylervion@proton.me>
2026-04-12 03:50:16 +0530

    Most aggressive minimal build: Only android.application plugin

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,10 +1,5 @@
-import com.android.build.api.variant.ApplicationVariant
-
 plugins {
     id("com.android.application")
-    kotlin("android") version "1.9.22"
-    kotlin("plugin.serialization") version "1.9.22"
-    kotlin("plugin.compose") version "1.5.11"
 }
 
 repositories {
@@ -57,67 +52,4 @@ android {
             signingConfig = signingConfigs.getByName("debug")
             applicationIdSuffix = ".debug"
         }
-        base.archivesBaseName = "VionBoard_" + defaultConfig.versionName
-        androidComponents.onVariants { variant: ApplicationVariant ->
-            if (variant.buildType == "debug") {
-                variant.androidResources.ignoreAssetsPatterns = listOf("main_ro.dict")
-                variant.proguardFiles = emptyList()
-                variant.proguardFiles.add(project.layout.buildDirectory.file(getDefaultProguardFile("proguard-android.txt").absolutePath))
-                variant.proguardFiles.add(project.layout.buildDirectory.file(project.buildFile.parent + "/proguard-rules.pro"))
-            }
-        }
-    }
-
-    buildFeatures {
-        viewBinding = true
-        buildConfig = true
-        compose = true
-    }
-
-    composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.11"
-    }
-
-    kotlinOptions {
-        jvmTarget = "17"
-    }
-
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
-    }
-
-    externalNativeBuild {
-        ndkBuild {
-            path = File("src/main/jni/Android.mk")
-        }
-    }
-
-    ndkVersion = "26.2.11394342"
-
-    packaging {
-        jniLibs {
-            useLegacyPackaging = true
-        }
-    }
-
-    testOptions {
-        unitTests {
-            isIncludeAndroidResources = true
-        }
-    }
-
-    dependenciesInfo {
-        includeInApk = false
-        includeInBundle = false
-    }
-
-    namespace = "helium314.keyboard.latin"
-    lint {
-        abortOnError = true
-    }
-}
-
-dependencies {
-    implementation("androidx.core:core-ktx:1.16.0")
-}
+        base.archivesName
